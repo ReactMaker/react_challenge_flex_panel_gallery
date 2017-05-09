6b85e7f05053553776dea3e97466610a5ac0f5f4
chnbohwr <chnbohwr@gmail.com>
2017-05-09 22:08:07 +0800

    add less and html

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -6,7 +6,33 @@ export default class Home extends Component {
   render() {
     return (
       <div id="pageHome">
-        this is home page
+        <div className="panels">
+          <div className="panel panel1">
+            <p>Hey</p>
+            <p>Lets</p>
+            <p>Dance</p>
+          </div>
+          <div className="panel panel2">
+            <p>Give</p>
+            <p>Take</p>
+            <p>Receive</p>
+          </div>
+          <div className="panel panel3">
+            <p>Experience</p>
+            <p>It</p>
+            <p>Today</p>
+          </div>
+          <div className="panel panel4">
+            <p>Give</p>
+            <p>All</p>
+            <p>You can</p>
+          </div>
+          <div className="panel panel5">
+            <p>Life</p>
+            <p>In</p>
+            <p>Motion</p>
+          </div>
+        </div>
       </div>
     );
   }
